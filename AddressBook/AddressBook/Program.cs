9c51546d11c81a9f96b156165d4a9e4f713dc0b9
harshadalohar123 <harshalohar123@gmail.com>
2022-03-20 16:25:22 +0530

    ADDED: UC_1_Address Book Management

---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To AddressBookManagement Program");
+            UC_1_Program uC_1_Program = new UC_1_Program();
+            PersonDetail person = new PersonDetail();
+            person.FirstName = "Harshada";
+            person.LastName = "Lohar";
+            person.Address = "Shirud Digar";
+            person.City = "Shahada";
+            person.State = "Maharastra";
+            person.Zip = 425409;
+            person.PhoneNumber = 35348632;
+            person.Email = "HarshaLohar@gmail.com";
+            uC_1_Program.addPerson(person);
+            uC_1_Program.Display();
+
+
+        }
+    }
+}
